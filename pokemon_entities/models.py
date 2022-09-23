e417f7d9c27fc2ca86e558ab3e1818252ac36e51
MrSabin <sabin.21011986@gmail.com>
2022-09-23 21:52:53 +0300

    Added description field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     photo = models.ImageField(upload_to="pokemons", null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -15,7 +16,7 @@ class PokemonEntity(models.Model):
     lat = models.FloatField()
     lon = models.FloatField()
     appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField()
+    disappeared_at = models.DateTimeField(default=timezone.now)
     level = models.IntegerField(null=True, blank=True)
     health = models.IntegerField(null=True, blank=True)
     strength = models.IntegerField(null=True, blank=True)
